cc1323f2a85c50591df1ede1a2b96f9f31820ade
james <james.speirs@gmail.com>
2018-12-15 07:43:37 -0700

    improve help, fix submit and init

--- client/index.js
+++ client/index.js
@@ -15,6 +15,10 @@ const args = Array.from(process.argv).slice(2)
     const client = await Client.login(url, sessionId)
     if (client) help(client)
 
+  // request for help
+  } else if (command === 'help') {
+    help(Client.load(), args[1])
+
   // if local cache has login info...
   } else {
     const client = Client.load()
@@ -24,13 +28,21 @@ const args = Array.from(process.argv).slice(2)
 
     // validate that the client session is valid
     } else if (!(await client.validateSession())) {
-      console.log('Credentials have expired. Please log in again.')
+      console.log('Credentials have expired. Please log in again.\n')
+      help(null)
 
     // init command
     } else if (command === 'init') {
       const challenge = args[1]
       const outputDir = args[2] ? path.resolve(process.cwd(), args[2]) : process.cwd()
-      client.initChallenge(challenge, outputDir)
+      if (challenge) {
+        client.initChallenge(challenge, outputDir)
+          .then(() => console.log('Downloaded challenge to ' + outputDir))
+          .catch(err => { console.error(err.message) })
+      } else {
+        console.log('Missing required <challenge> input\n')
+        help(client, 'init')
+      }
 
     // clear the client session
     } else if (command === 'logout') {
@@ -49,19 +61,34 @@ const args = Array.from(process.argv).slice(2)
           console.log(key)
           if (value.length) {
             value.forEach(v => {
-              console.log('  ' + (new Date(v.date)).toString() + '  ' + v.score)
+              console.log('  ' + (new Date(v.date)).toLocaleString() + '    ' + v.score)
             })
           } else {
             console.log('  Not submitted')
           }
         })
+      } else {
+        console.log('Unable to get status information')
       }
 
     // submit an assignment
     } else if (command === 'submit') {
       const challenge = args[1]
       const contentDir = args[2] ? path.resolve(process.cwd(), args[2]) : process.cwd()
-      client.submit(challenge, contentDir)
+      if (challenge) {
+        client.submit(challenge, contentDir)
+          .then(res => {
+            if (res.body && res.body.body) {
+              console.log(res.body.body)
+            } else {
+              console.log(res)
+            }
+          })
+          .catch(err => console.log(err.message))
+      } else {
+        console.log('Missing required <challenge> input\n')
+        help(client, 'submit')
+      }
 
     // unknown command
     } else if (command) {
@@ -74,30 +101,70 @@ const args = Array.from(process.argv).slice(2)
     }
   }
 
-  function help (client) {
+  function help (client, command) {
     const message = []
-    message.push('Usage:', '  challenge <command>', '', 'Status:')
-    if (client) {
-      message.push('  Logged in to ' + client.url)
-      message.push('  Logged in as ' + client.user)
-      message.push('')
-      message.push('Commands:')
-      message.push('  help                       Output this help')
-      message.push('  init <name> <output_dir>   Download a challenge to your computer')
-      message.push('  logout                     Log out of the client')
-      message.push('  status                     See challenges and completion status')
-      message.push('  submit <name> [dir]        Submit a challenge to the server')
+    if (!command || command === 'help') {
+      message.push('Usage:', '', '  challenge <command>', '', 'Status:', '')
+      if (client) {
+        message.push('  Logged in to ' + client.url)
+        message.push('  Logged in as ' + client.user)
+        message.push('')
+        message.push('Commands:', '')
+        message.push('  help [command]                 Output help')
+        message.push('  init <challenge> [output_dir]  Download a challenge from the challenger server')
+        message.push('  logout                         Log out of the client')
+        message.push('  status                         See challenges and completion status')
+        message.push('  submit <challenge> [dir]       Submit a challenge to the server')
+      } else {
+        message.push('  Please log in')
+        message.push('')
+        message.push('Commands:', '')
+        message.push('  help [command]                 Output help')
+        message.push('  login <remote_url> <sid>   Log in to the challenge server')
+      }
+    } else if (client) {
+      switch (command) {
+        case 'init':
+          //           '123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 '
+          message.push('Synopsis:', '', '  Download a challenge from the challenger server', '')
+          message.push('Usage:', '', '  challenge init <challenge> [dir]', '', 'Arguments:', '')
+          message.push('  challenge    The name of the challenge to download')
+          message.push('  dir          The directory to download to (defaults to current directory)')
+          break
+        case 'logout':
+          message.push('Synopsis:', '', '  Log out of the client', '')
+          message.push('Usage:', '', '  challenge logout')
+          break
+        case 'status':
+          message.push('Synopsis:', '', '  See challenges and completion status', '')
+          message.push('Usage:', '', '  challenge status')
+          break
+        case 'submit':
+          message.push('Synopsis:', '', '  Submit a challenge to the server', '')
+          message.push('Usage:', '', '  submit <challenge> [dir]', '', 'Arguments:', '')
+          message.push('  challenge    The name of the challenge to upload')
+          message.push('  dir          The directory upload from (defaults to current directory)')
+          break
+        default:
+          message.push('Command does not exist: ' + command)
+          break
+      }
     } else {
-      message.push('  Please log in')
-      message.push('')
-      message.push('Commands:')
-      message.push('  help                       Output this help')
-      message.push('  login <remote_url> <sid>   Log in to the challenge server')
+      switch (command) {
+        case 'login':
+          message.push('Usage:', '  challenge login <remote_url> <sid>', '', 'Arguments:')
+          message.push('  remote_url   The URL to use to communicate with the challenge server.')
+          message.push('  sid          The session id to log into the challenge server with')
+          message.push('', 'The website hosting the challenge server should have this information on it')
+          break
+      }
     }
-    message.push('')
 
+    message.push('')
     while (message.length) {
       console.log(message.shift())
     }
   }
-})()
+})().catch(err => {
+  console.error(err)
+})
