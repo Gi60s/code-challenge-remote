7ae637ecec748b1aa005e94c5af14759a0986d74
james <james.speirs@gmail.com>
2018-12-15 07:42:19 -0700

    produce error if stream too long

--- middleware/stream-meter.js
+++ middleware/stream-meter.js
@@ -13,6 +13,10 @@ inherits(StreamMeter, Transform)
 StreamMeter.prototype._transform = function (chunk, encoding, cb) {
   this.bytes += chunk.length
   this.push(chunk)
-  if (this.bytes > this.maxBytes) return cb(new Error('Stream size exceeded maximum allowed'))
+  if (this.bytes > this.maxBytes) {
+    const err = Error('Stream size exceeded maximum allowed')
+    err.code = 'ECLIENTREQ'
+    return cb(err)
+  }
   cb()
 }
