12dff2e63c7bc1243fb5d6e1d20e26a8199f19bf
james <james.speirs@gmail.com>
2018-12-19 17:04:14 -0700

    update how the parser works for docker-compose

--- example/challenges/first-challenge/hooks.js
+++ example/challenges/first-challenge/hooks.js
@@ -9,12 +9,29 @@ exports.afterOverwrite = function (uploadedFilesDir, overwriteFilesDir) {
 
 exports.parseTestResults = function (output) {
   const ar = output.split('== BEGIN TESTS ==')
-  const body = ar[1] || 'Tests failed to run'
-  const passed = /^\s*(\d+) passing/m.exec(body)
-  const failed = /^\s*(\d+) failing/m.exec(body)
-  return {
-    body,
-    failed: failed ? +failed[1] : 0,
-    passed: passed ? +passed[1] : 0
+  if (ar[1]) {
+    const content = ar[1]
+      .split(/\r\n|\r|\n/)
+      .filter(v => v.startsWith('app_1'))
+      .map(v => {
+        const ar = v.split('|')
+        ar.shift()
+        return ar.join('|')
+      })
+      .join('\n')
+
+    const passed = /^\s*(\d+) passing/m.exec(content)
+    const failed = /^\s*(\d+) failing/m.exec(content)
+    return {
+      body: content,
+      failed: failed ? +failed[1] : 0,
+      passed: passed ? +passed[1] : 0
+    }
+  } else {
+    return {
+      body: 'Tests failed to run',
+      failed: 0,
+      passed: 0
+    }
   }
 }
